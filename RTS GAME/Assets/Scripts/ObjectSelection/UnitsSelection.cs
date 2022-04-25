2de22bb863f2d90c11e65a2316b81a92e6f1b8d2
Кріп Степан <62791558+DedBibon@users.noreply.github.com>
2022-04-25 18:30:32 +0300

    final box selection script
    Скрипт створення зони виділу.
    Видалення непотрібних матеріалів.

--- RTS GAME/Assets/Scripts/ObjectSelection/UnitsSelection.cs
+++ RTS GAME/Assets/Scripts/ObjectSelection/UnitsSelection.cs
@@ -7,76 +7,39 @@ namespace ObjectSelection
     public class UnitsSelection : MonoBehaviour
     {
         public float thickness;
-        public LayerMask clickable;
-        public LayerMask ground;
-
         private bool _isSelecting;
-        
         private static Vector3 _mousePositing;
         
         private void Update()
         {
+            //Start box selection
             if (Input.GetMouseButtonDown(0))
             {
                 _isSelecting = true;
                 _mousePositing = Input.mousePosition;
-
+                
+                //Deactivation of the camera control script
                 gameObject.GetComponent<CameraControlKeyboard>().enabled = false;
             }
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                _isSelecting = false;
-                
-                gameObject.GetComponent<CameraControlKeyboard>().enabled = true;
-       
-                // Camera myCam = Camera.main;
-                // RaycastHit hit;
-                // Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
-                // if (Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
-                // {
-                //     UnitManager.Selected(hit.collider.gameObject);
-                // }
-            }
-
-            if (_isSelecting)
-            {
-                // foreach (var obj in FindObjectsOfType<UnitComponent>())
-                // {
-                //     
-                //     if (Input.GetKey(KeyCode.LeftShift))
-                //     {
-                //         if (IsWithinSelectionBounds(obj.gameObject))
-                //          UnitManager.Selected(obj.gameObject);
-                //     }
-                //     else
-                //     {
-                //         if (IsWithinSelectionBounds(obj.gameObject))
-                //             UnitManager.Selected(obj.gameObject);
-                //         else
-                //             UnitManager.DeSelected(obj.gameObject);
-                //     }
-                // }
-            }
+            
+            //End box selection
+            if (!Input.GetMouseButtonUp(0)) return;
+            
+            _isSelecting = false;
+            
+            //Activation of thecamera contol script
+            gameObject.GetComponent<CameraControlKeyboard>().enabled = true;
         }
 
         private void OnGUI()
         {
             if (!_isSelecting) return;
-
+            
+            //Draw rect box selection
             var rect = DrawBoxSelection.GetScreenRect(_mousePositing, Input.mousePosition);
             DrawBoxSelection.DrawRect(rect, new Color(0.5f, 1f, 0.4f, 0.2f));
             DrawBoxSelection.DrawScreenRectBorder(rect, thickness, new Color(0.5f, 1f, 0.4f));
         }
-
-        //Checking the entry of the object into the box selection
-        private static bool IsWithinSelectionBounds(GameObject gameObject)
-        {
-            var camera = Camera.main;
-            var viewportBounds =
-                DrawBoxSelection.GetViewportBounds(camera, _mousePositing, Input.mousePosition);
-
-            return viewportBounds.Contains(camera.WorldToViewportPoint(gameObject.transform.position));
-        }
+        
     }
 }
