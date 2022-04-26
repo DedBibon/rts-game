f419b2a192c8c36dce307d012ac2a42900ada09b
Кріп Степан <62791558+DedBibon@users.noreply.github.com>
2022-04-27 00:37:04 +0300

    update unit selection
    Додано метод перевірки входження об'єкту в зону виділу (IsWithinSelectionBounds)
    Додано метод виділення підконтрольних методів (CheckSelectionObject)

--- RTS GAME/Assets/Scripts/ObjectSelection/UnitsSelection.cs
+++ RTS GAME/Assets/Scripts/ObjectSelection/UnitsSelection.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using BoxSelection;
 using CameraControl;
+using Units;
 
 namespace ObjectSelection
 {
     public class UnitsSelection : MonoBehaviour
     {
+        [SerializeField] private Team team;
+
         public float thickness;
         private bool _isSelecting;
         private static Vector3 _mousePositing;
-        
+
         private void Update()
         {
             //Start box selection
@@ -17,16 +20,19 @@ namespace ObjectSelection
             {
                 _isSelecting = true;
                 _mousePositing = Input.mousePosition;
-                
+
                 //Deactivation of the camera control script
                 gameObject.GetComponent<CameraControlKeyboard>().enabled = false;
             }
             
+            //Check the contents of the object in the selection area
+            CheckSelectionObject();
+
             //End box selection
-            if (!Input.GetMouseButtonUp(0)) return;
-            
+            if (!Input.GetMouseButtonUp(0)) 
+                return;
             _isSelecting = false;
-            
+
             //Activation of thecamera contol script
             gameObject.GetComponent<CameraControlKeyboard>().enabled = true;
         }
@@ -34,12 +40,54 @@ namespace ObjectSelection
         private void OnGUI()
         {
             if (!_isSelecting) return;
-            
+
             //Draw rect box selection
             var rect = DrawBoxSelection.GetScreenRect(_mousePositing, Input.mousePosition);
             DrawBoxSelection.DrawRect(rect, new Color(0.5f, 1f, 0.4f, 0.2f));
             DrawBoxSelection.DrawScreenRectBorder(rect, thickness, new Color(0.5f, 1f, 0.4f));
         }
-        
+
+        private static bool IsWithinSelectionBounds(GameObject gameObject)
+        {
+            var camera = Camera.main;
+            var viewportBounds =
+                DrawBoxSelection.GetViewportBounds(camera, _mousePositing, Input.mousePosition);
+
+            return camera != null &&
+                   viewportBounds.Contains(camera.WorldToViewportPoint(gameObject.transform.position));
+        }
+
+        private void CheckSelectionObject()
+        {
+            if (!_isSelecting)
+                return;
+            
+            var objWay = gameObject.GetComponent<UnitManager>();
+            foreach (var list in objWay.AllUnitsPlayer)
+            {
+                //Choice with Shift and without
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    if (IsWithinSelectionBounds(list.gameObject))
+                        objWay.AddUnits(list.gameObject);
+                }
+                else
+                {
+                    if (IsWithinSelectionBounds(list.gameObject))
+                        objWay.AddUnits(list.gameObject);
+                    else
+                        objWay.DelUnits(list.gameObject);
+                }
+                objWay.InfoSelection();
+            }
+        }
+
+        public Team GetTeam => team;
+
+        public enum Team
+        {
+            Green,
+            Red
+        }
     }
 }
